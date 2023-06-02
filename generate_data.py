243a8adba0edac4f16b920934daef6de76f63f4f
Dan Le Man <holydoge1337@gmail.com>
2023-06-01 18:45:16 -0700

    Improved 'generate_data' by considering dividends and annualizing over a 13 month period beginning from the current time. Added a 'volatile' partition of 'b', as opposed to the original 'uniform' partition.

--- generate_data.py
+++ generate_data.py
@@ -2,12 +2,14 @@ import pandas as pd
 import yfinance as yf
 import csv
 from datetime import datetime, timedelta
+from pytz import timezone
 from statistics import stdev
 
 
 YEARS_TO_MEASURE = 5 # how many years to go back to compute data. either 1, 5, or 10.
-START = datetime.now() - timedelta(days = 365 * YEARS_TO_MEASURE) # starting time. 
-# will be off by a few days due to leap year, however only year is used. just don't run this around new years.
+START = datetime.now() - timedelta(days = 365 * YEARS_TO_MEASURE) # will be off by a few days due to leap year. this should not be too problematic
+CURRENT_MONTH = datetime.now().month # this script WILL not work if ran in December. see 'current_year_data' variable.
+NY_TZ = timezone('America/New_York')
 
 
 securities_table = {}
@@ -18,31 +20,38 @@ with open('securities.csv') as securities_file:
 
 roi_data = {}
 
-for category, list in securities_table.items():
+for basket, list in securities_table.items():
     securities = yf.Tickers(list)
     for name, tick in securities.tickers.items():
         if name in roi_data:
-            roi_data[name]['Category'] = roi_data[name]['Category'] + f', {category}'
+            roi_data[name]['Basket'] = roi_data[name]['Basket'] + f', {basket}'
             continue
-
-        hist = tick.history(period=f'{YEARS_TO_MEASURE}y', interval='1mo')
+        hist = tick.history(period=f'{YEARS_TO_MEASURE}y', interval='1mo', start=f'{START.year}-01-01')
         hist['Average'] = hist[['Open', 'Close']].mean(axis=1)
         annual_returns = []
-        for i in range(YEARS_TO_MEASURE + 1):
-            current_year_data = hist.filter(
-                like=str((START + timedelta(days=365 * i)).year),
-                axis=0
-            )
+        confidence = 0
+        for i in range(YEARS_TO_MEASURE, 0, -1):
+            current_year_data = hist[
+                datetime((START + timedelta(days=365 * (i - 1))).year, CURRENT_MONTH - 1, 1, tzinfo=NY_TZ) :
+                datetime((START + timedelta(days=365 * i)).year, CURRENT_MONTH, 1, tzinfo=NY_TZ)
+            ]
             if current_year_data.empty:
                 continue
-
+            
+            confidence += len(current_year_data) - 1
             annualized_monthly_return = 1
             for mo in range(1, len(current_year_data)):
                 prev_month = current_year_data.iloc[mo-1]
                 curr_month = current_year_data.iloc[mo]
-                annualized_monthly_return *= curr_month['Average'] / prev_month['Average']
-            annual_returns.append(annualized_monthly_return)
 
+                start_of_month = datetime(curr_month.name.year, curr_month.name.month, 1, tzinfo=NY_TZ)
+                end_of_month = start_of_month.replace(day=28) + timedelta(days=4)
+                end_of_month = end_of_month - timedelta(days=end_of_month.day)
+                div = tick.dividends[start_of_month : end_of_month]
+                
+                annualized_monthly_return *= (curr_month['Average'] + div.sum()) / prev_month['Average']
+            annual_returns.append(annualized_monthly_return)
+            
         try:
             avg_ann_return = sum(annual_returns) / len(annual_returns)
         except ZeroDivisionError:
@@ -51,8 +60,9 @@ for category, list in securities_table.items():
         roi_data[name] = {
             'Annual Return': avg_ann_return - 1,
             'Volatility': stdev(annual_returns, xbar=avg_ann_return),
+            'Confidence': confidence / (12 * YEARS_TO_MEASURE),
             'Name': tick.info['shortName'],
-            'Category': category
+            'Basket': basket
         }
 
 roi_data = pd.DataFrame(roi_data).T
--- model.py
+++ model.py
@@ -7,35 +7,49 @@ AMOUNT_TO_INVEST = 5000
 
 data = pd.read_csv('data.csv', index_col=0)
 data['Annual Return'] = data['Annual Return'] + 1
-data['Volatility'] = data['Volatility'] + 1
+# data['Volatility'] = data['Volatility'] + 1
 
 
-categories = list(dict.fromkeys(', '.join(data['Category'].unique()).split(', ')))
+baskets = list(dict.fromkeys(', '.join(data['Basket'].unique()).split(', ')))
 variables = data.index.tolist()
 
 c = -1 * data['Annual Return'].to_numpy(dtype=np.float64)
 
-A = np.empty((len(categories) + 1, len(data)), dtype=np.float64)
+A = np.empty((len(baskets) + 1, len(variables)), dtype=np.float64)
 
-for i in range(len(categories)):
-    category = categories[i]
+for i in range(len(baskets)):
+    basket = baskets[i]
     for j in range(len(variables)):
         var = variables[j]
-        A[i, j] = data['Volatility'][var] if category in data['Category'][var] else 0
+        A[i, j] = (1 / data['Confidence'][var]) * data['Volatility'][var] if basket in data['Basket'][var] else 0
+        # A[i, j] = (1 / data['Confidence'][var]) if basket in data['Basket'][var] else 0
 
-A[len(categories)] = np.ones(shape=len(variables), dtype=np.float64)
 
+A[len(baskets)] = np.ones(shape=len(variables), dtype=np.float64)
+
+# "uniform" partition of b
 zeros = np.count_nonzero(A, axis=1)
 zeros[-1] = 0
-b = zeros * (AMOUNT_TO_INVEST / sum(zeros))
-b[-1] = AMOUNT_TO_INVEST
+b_uniform = zeros * (AMOUNT_TO_INVEST / sum(zeros))
+b_uniform[-1] = AMOUNT_TO_INVEST
 
+# "volatile" partition of b
+b_volatile = np.empty(shape=len(baskets) + 1, dtype=np.float64)
+b_volatile[-1] = AMOUNT_TO_INVEST 
 
-result = linprog(c, A_ub = A, b_ub = b)
+for i in range(len(baskets)):
+    average_volatility = data[data['Basket'].str.contains(baskets[i])]['Volatility'].mean()
+    b_volatile[i] = (average_volatility) * AMOUNT_TO_INVEST
+
+result = linprog(c, A_ub = A, b_ub = b_volatile)
+
+# print(result)
 
 formatted_result = {}
 
 for i in range(len(variables)):
-    formatted_result[variables[i]] = result.x[i]
+    formatted_result[data['Name'][variables[i]]] = result.x[i]
 
 print(formatted_result)
+# print(b_volatile)
+
